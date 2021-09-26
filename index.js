e7b1448464ce40fe399e6f5a668dfaeacc570e10
KrisztianCsillag <kcsillag@maxedy.com>
2021-09-26 16:32:35 +0200

    Added details to the Procfile

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const axieListRouter = require("./routers/axie-list-router");
 const axieDetailsRouter = require("./routers/axie-details-router");
 
-app.set("port", process.env.PORT || 4000);
+const PORT = process.env.PORT || 4000;
 
 app.use(cors());
 app.use(express.json());
@@ -12,6 +12,4 @@ app.use(express.json());
 app.use("/axielist", axieListRouter);
 app.use("/axiedetails", axieDetailsRouter);
 
-app.listen(app.get("port"), (server) => {
-  console.info(`Server listen on port ${app.get("port")}`);
-});
+app.listen(PORT);
