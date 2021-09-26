7c5146f68f703a799b1ba8aef2a96ea21926156d
KrisztianCsillag <kcsillag@maxedy.com>
2021-09-26 16:24:38 +0200

    hopefully ready for deployment

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const axieListRouter = require("./routers/axie-list-router");
 const axieDetailsRouter = require("./routers/axie-details-router");
 
-app.set("port", process.env.port || 4000);
+app.set("port", process.env.PORT || 4000);
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/axielist", axieListRouter);
